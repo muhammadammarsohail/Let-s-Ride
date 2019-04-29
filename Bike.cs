9bff8d525c760102b303609d3a2db26c01a9fd75
Ammar Sohail <muhammadammarsohail@gmail.com>
2019-04-29 23:41:23 +0500

    Make an interface IVehicle, a class Bike inherits IVehicle and two classes Motorcycle and Bicycle inherits the Bike class. Implement the Speedup method in the bike class

---
+++ Bike.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ride
+{
+    class Bike : IVehicle
+    {
+        public int Speedup(int initialSpeed, int intensity)
+        {
+            int finalSpeed = initialSpeed + intensity * 5;
+            return finalSpeed;
+        }
+    }
+}
---
+++ IVehicle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ride
+{
+    interface IVehicle
+    {
+        int Speedup(int initialSpeed, int intensity);
+        int ApplyBrakes(int initialSpeed, int intensity);
+        int ChangeGear(int initialGear, int change);
+        int Display(int gear, int speed);
+    }
+}
