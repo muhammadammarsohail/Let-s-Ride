1b0a83f4c5265c89b955b176841a2024dcf5ecb5
Ammar Sohail <muhammadammarsohail@gmail.com>
2019-04-30 00:13:11 +0500

    The Go Function Should be in the parent class Bike.

--- Bicycle.cs
+++ Bicycle.cs
@@ -8,21 +8,6 @@ namespace Ride
 {
     class Bicycle : Bike
     {
-        public void Go()
-        {
-            string action = Console.ReadLine();
-            char act = action[0];
-            switch (act)
-            {
-                case 'r':
-                    break;
-                case 'b':
-                    break;
-                case 'u':
-                    break;
-                case 'd':
-                    break;
-            }
-        }
+        
     }
 }
--- Bike.cs
+++ Bike.cs
@@ -9,6 +9,22 @@ namespace Ride
     class Bike : IVehicle
     {
         public int speed, gear;
+        public void Go()
+        {
+            string action = Console.ReadLine();
+            char act = action[0];
+            switch (act)
+            {
+                case 'r':
+                    break;
+                case 'b':
+                    break;
+                case 'u':
+                    break;
+                case 'd':
+                    break;
+            }
+        }
         public int Speedup(int initialSpeed, int intensity)
         {
             int finalSpeed = initialSpeed + intensity * 5;
