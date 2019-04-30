66d92485212ea24b3e9a2a974391335db1c3ba93
Ammar Sohail <muhammadammarsohail@gmail.com>
2019-04-30 08:37:14 +0500

    Implement the Go function in the Bike class.

--- Bike.cs
+++ Bike.cs
@@ -11,29 +11,43 @@ namespace Ride
         public int speed, gear;
         public void Go()
         {
+            do {
             string action = Console.ReadLine();
             char act = action[0];
-            switch (act)
-            {
-                case 'r':
-                    break;
-                case 'b':
-                    break;
-                case 'u':
-                    break;
-                case 'd':
-                    break;
-            }
+                if (act == 's') break;
+                switch (act)
+                {
+                    case 'r':
+                        int intensityR = action.Length * 5;
+                        speed = Speedup(speed, intensityR);
+                        break;
+                    case 'b':
+                        int intensityBr = action.Length * 5;
+                        speed = ApplyBrakes(speed, intensityBr);
+                        break;
+                    case 'u':
+                        int shiftU = action.Length;
+                        gear = ChangeGear(gear, shiftU);
+                        break;
+                    case 'd':
+                        int shiftD = 0 - action.Length;
+                        gear = ChangeGear(gear, shiftD);
+                        break;
+                    default:
+                        Console.WriteLine("Put a valid action.");
+                        break;
+                }  
+            } while (true);
         }
         public int Speedup(int initialSpeed, int intensity)
         {
-            int finalSpeed = initialSpeed + intensity * 5;
+            int finalSpeed = initialSpeed + intensity;
             return finalSpeed;
         }
 
         public int ApplyBrakes(int initialSpeed, int intensity)
         {
-            int finalSpeed = initialSpeed - intensity * 5;
+            int finalSpeed = initialSpeed - intensity;
             return finalSpeed;
         }
 
